5e28eb19dd6e9b13e27339636d3c8017aa1f742f
Spartix <nicolasmacrez999@gmail.com>
2021-12-01 18:30:35 +0100

    Ajout : API food/coffee

---
+++ src/commands/cofee.js
@@ -0,0 +1,46 @@
+const { Command } = require('discord-akairo');
+const { PREFIX } = require('../../config');
+const fetch = import('node-fetch');
+const moment = require('moment');
+
+class CoffeeCommand extends Command {
+    constructor() {
+        super('coffee', {
+           aliases: ['coffee'],
+           description: {
+            content: 'La commande coffee permet d\'envoyer une image de caffé',
+            usages: `${PREFIX}coffee`,
+            raccourcis: 'coffee'
+        },
+           category: 'API',
+           ignoreCooldown: '831835872625295390',
+           ignorePermissions: '831835872625295390',
+           userPermissions: 'SEND_MESSAGES',
+           clientPermissions: 'SEND_MESSAGES',
+           ratelimit: 2,
+           cooldown: 3000,
+           typing: true,
+           ownerOnly: false,
+           channel: 'guild',
+        });
+    }
+
+    async exec(message, args) {
+        let CONSOLE_LOG_CHANNEL = this.client.channels.cache.get('915642499077402668')
+        const { NekoBot } = require("nekobot-api");
+        const api = new NekoBot();
+
+            const coffee = await api.get("coffee");
+            message.channel.send({ embeds: [ 
+                this.client.functions.embed() 
+                    .setTitle(`***Voici votre image !***`)
+                    .setURL(coffee)
+                    .setImage(`${coffee}`)
+                    .setFooter(`demandé par ${message.member.user.tag}`)
+                    .setColor('#D518C9')
+            ]})
+            CONSOLE_LOG_CHANNEL.send(`${'```'}${moment().format('LTS')} : ${message.member.user.tag} a exécuté la commande ${message.content} ${'```'}`)
+        }
+}
+
+module.exports = CoffeeCommand;
---
+++ src/commands/food.js
@@ -0,0 +1,46 @@
+const { Command } = require('discord-akairo');
+const { PREFIX } = require('../../config');
+const fetch = import('node-fetch');
+const moment = require('moment');
+
+class FoodCommand extends Command {
+    constructor() {
+        super('food', {
+           aliases: ['food'],
+           description: {
+            content: 'La commande food permet d\'envoyer une image de caffé',
+            usages: `${PREFIX}food`,
+            raccourcis: 'food'
+        },
+           category: 'API',
+           ignoreCooldown: '831835872625295390',
+           ignorePermissions: '831835872625295390',
+           userPermissions: 'SEND_MESSAGES',
+           clientPermissions: 'SEND_MESSAGES',
+           ratelimit: 2,
+           cooldown: 3000,
+           typing: true,
+           ownerOnly: false,
+           channel: 'guild',
+        });
+    }
+
+    async exec(message, args) {
+        let CONSOLE_LOG_CHANNEL = this.client.channels.cache.get('915642499077402668')
+        const { NekoBot } = require("nekobot-api");
+        const api = new NekoBot();
+
+            const food = await api.get("food");
+            message.channel.send({ embeds: [ 
+                this.client.functions.embed() 
+                    .setTitle(`***Voici votre image !***`)
+                    .setURL(food)
+                    .setImage(`${food}`)
+                    .setFooter(`demandé par ${message.member.user.tag}`)
+                    .setColor('#D518C9')
+            ]})
+            CONSOLE_LOG_CHANNEL.send(`${'```'}${moment().format('LTS')} : ${message.member.user.tag} a exécuté la commande ${message.content} ${'```'}`)
+        }
+}
+
+module.exports = FoodCommand;
